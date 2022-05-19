0bb1c45c8d5e896f4858f441452eea8793ef9a04
artur93mCourseProject <artur93michalak@wp.pl>
2022-05-19 22:23:04 +0200

    Use process.argv to fetch data for city and show weather data in console

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
+const cityName = process.argv[2];
 
-fetch('https://wp.pl').then(r => r.text()).then(html => console.log(html))
+const processWeatherData = data => {
+    const foundData = data.find(stationData => stationData.stacja === cityName);
+
+    if (!foundData) {
+        console.log('Takiego miasta nasze Api nie przewidziało :(')
+        return;
+    }
+
+    const {
+        cisnienie: pressure,
+        wilgotnosc_wzgledna: humidity,
+        temperatura: temperature
+    } = foundData;
+
+    const weatherInfo = `In ${cityName} there is ${temperature}C, ${humidity}% of humidity and pressure of ${pressure} hPa`;
+    console.log(weatherInfo)
+};
+
+fetch('https://danepubliczne.imgw.pl/api/data/synop')
+    .then(r => r.json())
+    .then(processWeatherData);
+
+
+// https//danepubliczne.imgw.pl/api/data/synop
